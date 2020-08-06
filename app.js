eef09959b6ce71139b3f9d38612f413bbf4a2153
Phoebe <jolinhappy1990@hotmail.com>
2020-08-06 17:35:56 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ app.post('/records/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//delete
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 
 app.listen(port, () => {
